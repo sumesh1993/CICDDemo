77e068b391185e2b3db38305f8272609a70faa9d
isumesh <63849019+isumesh@users.noreply.github.com>
2022-01-28 11:05:30 +0530

    Add: Text

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Welcome to my app</Text>
+      <Text>This is another Text</Text>
       <StatusBar style="auto" />
     </View>
   );
